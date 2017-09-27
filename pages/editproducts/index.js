6dbfb6379fa8fbb031cf276832a60b354cdd60b5
lihuang <517306208@qq.com>
2017-09-27 10:01:49 +0800

    修复编辑新闻/产品简介过多时内容遮盖底部按钮

--- pages/editproducts/index.js
+++ pages/editproducts/index.js
@@ -251,7 +251,7 @@ Page({
           category_id: res.data.info.category.length >0 ? res.data.info.category[0].id : '',
           category_name: res.data.info.category.length > 0 ? res.data.info.category[0].name : '',
           info: res.data.info.info,
-          coverImgSrc: res.data.info.res[0].res_url,
+          coverImgSrc: res.data.info.res.length > 0 ? res.data.info.res[0].res_url : '',
           res_id: res.data.info.category.length > 0 ? res.data.info.res[0].res_id : '',      
           synopsis: res.data.info.synopsis,
           title: res.data.info.title
